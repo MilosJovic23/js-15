271df7628eaac04622368f125fb18fec47cc130c
MilosJovic23 <milos.mj992@gmail.com>
2024-11-21 11:46:38 +0100

    * initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//
+
+//
+
+$(document).ready(function () {
+	// POST GET
+
+	$.ajax({
+		url: "https://reqres.in/api/users?page=2",
+		success: function (response) {
+			for (let user in response.data) {
+				var email = response.data[user].email;
+				var ime = response.data[user].first_name;
+				$("#imeLista").append("<p>" + ime + ":" + email + "</p>");
+			}
+		},
+	});
+
+	$.ajax({
+		url: "https://reqres.in/api/users",
+		type: "POST",
+		data: {
+			name: "Toma",
+			job: "programer",
+		},
+		success: function (response) {
+			// console.log(response);
+		},
+	});
+
+	$("#delete").click(function () {
+		$.ajax({
+			url: "https://reqres.in/api/users/2",
+			type: "DELETE",
+			success: function (response) {
+				console.log("test");
+				response;
+			},
+		});
+	});
+
+	$("#registracija").click(function () {
+		var ime = $("#name").val();
+		var imejl = $("#email").val();
+		$.ajax({
+			url: "https://reqres.in/api/users",
+			type: "POST",
+            data:{
+                name:ime,
+                email: imejl
+            },
+            success: function (response){
+                console.log(response)
+            }
+			
+		});
+	});
+});
+
+// $("#imeImejl").text(response.data.email+response.data.first_name)
